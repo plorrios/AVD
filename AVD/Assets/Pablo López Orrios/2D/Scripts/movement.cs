e2da5b51fd161a01fa0263924f783c7c023c3441
plorrios <pablolopezorrios@hotmail.es>
2020-02-14 19:23:25 +0100

    commit 1 friday 14th february

---
+++ AVD/Assets/Pablo López Orrios/2D/Scripts/movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    public float runSpeed = 40f;
+    [SerializeField]
+    float horizontalMove = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { rigidbody.AddForce( new Vector2(0,100)); }
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+    }
+}
