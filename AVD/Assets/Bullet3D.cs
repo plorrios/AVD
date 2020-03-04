11752991fcee94d571e89fcd376c9cc35e2d3f56
plorrios <pablolopezorrios@hotmail.es>
2020-03-04 16:33:28 +0100

    Commit Wednesday 4 March

--- AVD/Assets/Bullet3D.cs
+++ AVD/Assets/Bullet3D.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class Bullet3D : MonoBehaviour
 {
+    public LayerMask layermask;
     public float speed = 60f;
-    public Rigidbody rb;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Awake()
     {
-        rb.AddRelativeForce(-transform.forward*speed);
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(-transform.forward * speed, ForceMode.Impulse);
+        Destroy(gameObject, 3f);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    public void Die() 
     {
-        if (!collision.CompareTag("Player")) { Destroy(collision.gameObject); Destroy(gameObject); }
+        Destroy(gameObject);
+    }
 
-
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (layermask == (layermask | (1 << collision.gameObject.layer)))
+        {
+            rb.Sleep();
+            GetComponent<Animator>().SetTrigger("Die");
+        }
     }
 }
--- AVD/Assets/Pablo López Orrios/Materials and Ilumination/Scripts/Shoot.cs
+++ AVD/Assets/Pablo López Orrios/Materials and Ilumination/Scripts/Shoot.cs
@@ -7,37 +7,30 @@ public class Shoot : MonoBehaviour
     public GameObject bullet;
 
     public bool shooting = true;
+    public float frequency = 1;
 
-    public Transform shootingPoint, shootingPoint2, shootingPoint3, shootingPoint4;
+    public Transform[] shootingPoints;
+    public Animator[] GunsAnimators;
 
     public void Comienza()
     {
         StartCoroutine(Fire());
     }
-
+    private int i = 0;
     IEnumerator Fire()
     {
-        shooting = true;
+        GunsAnimators[i].SetTrigger("Fire");
 
-        Instantiate(bullet, shootingPoint.position, shootingPoint.rotation);
-        yield return new WaitForSeconds(0.3f);
-        
-        Instantiate(bullet, shootingPoint2.position, shootingPoint2.rotation);
-        yield return new WaitForSeconds(0.3f);
-
-        Instantiate(bullet, shootingPoint3.position, shootingPoint3.rotation);
-        yield return new WaitForSeconds(0.3f);
-
-        Instantiate(bullet, shootingPoint4.position, shootingPoint4.rotation);
-        yield return new WaitForSeconds(0.3f);
-
-
-       // if (shooting) StartCoroutine(Fire());
+        Instantiate(bullet, shootingPoints[i].position, shootingPoints[i].rotation);
+        i++;
+        if (i >= shootingPoints.Length) i = 0;
+        yield return new WaitForSeconds(frequency);
+        StartCoroutine(Fire());
     }
 
-    public void stopShooting() { 
-        shooting = false; 
-        StopCoroutine(Fire());
+    private void OnDisable()
+    {
+        StopAllCoroutines();
     }
 
 }
